40c73fe684f2099273fbbe7212147a10075ecaa5
benjaminherman <benjaminherman97@gmail.com>
2019-08-21 19:57:10 -0700

    Fix kickMessage return

--- src/main/java/net/illager/plugin/timeout/App.java
+++ src/main/java/net/illager/plugin/timeout/App.java
@@ -2,11 +2,9 @@ package net.illager.plugin.timeout;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
-import org.bukkit.event.Event;
 import java.io.File;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 
 public class App extends JavaPlugin {
 
@@ -25,27 +23,27 @@ public class App extends JavaPlugin {
     
     public static final long TIMEOUT = 72 * 3600 * 1000;
     
-    public static String kickMessage(long timeout) {
+    public static String kickMessage(long timeout) {        
         String remaining = "";
-        long days = timeout / 24 * 3600 * 1000;
-        long hours = timeout % days / 3600 * 1000;
-        long minutes = timeout % days % hours / 60 * 1000;
-        long seconds = timeout % days % hours % minutes / 1000;
+        long days = timeout / 86400000;
+        long hours = timeout % 86400000 / 3600000;
+        long minutes = timeout % 3600000 / 60000;
+        long seconds = timeout % 60000 / 1000;
         
         if(days > 0) {
-            remaining = days + (days > 1 ? " days": " day") + hours + (hours > 1 ? " hours": "hour");
+            remaining = days + " " + (days > 1 ? "days": "day") + " " + hours + " " + (hours > 1 ? "hours": "hour");
         }
         
         else if(hours > 0) {
-            remaining = hours + (hours > 1 ? " hours": "hour") + minutes + (minutes > 1 ? " minutes": " minute");
+            remaining = hours + " " + (hours > 1 ? "hours": "hour") + " " + minutes + " " + (minutes > 1 ? "minutes": "minute");
         }
         
         else if(minutes > 0) {
-            remaining = minutes + (minutes > 1 ? " minutes": " minute") + seconds + (seconds > 1 ? " seconds": "second");
+            remaining = minutes + " " + (minutes > 1 ? "minutes": "minute") + " " + seconds + " " + (seconds > 1 ? "seconds": "second");
         }
         
         else {
-            remaining = seconds + (seconds > 1 ? " seconds": "second");
+            remaining = seconds + " " + (seconds > 1 ? "seconds": "second");
         }
         
         return remaining;
