bede141d13c06ee014c58e489c0c070b7efac35a
Benjamin Herman <benjaminherman97@gmail.com>
2019-10-11 13:24:02 -0700

    Remove maven as dependancy

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+spigot='spigot-1.14.4.jar'
+name='Timeout'
+version='1.0.0'
+
+# Download dependencies
+mkdir -p lib
+if test ! -f lib/$spigot; then
+    echo "Downloading Spigot"
+    curl "https://cdn.getbukkit.org/spigot/$spigot" --output lib/$spigot
+fi
+
+# Compile plugin
+echo "Compiling plugin"
+rm -rf bin
+mkdir bin
+javac $(find src -name "*.java") \
+    -classpath lib/$spigot \
+    -source 8 \
+    -target 8 \
+    -d bin
+if [ $? -eq 1 ]; then
+    exit 1
+fi
+
+# Archive plugin
+echo "Packaging plugin"
+jar -cvf $name-$version.jar \
+    -C res . \
+    -C bin .
